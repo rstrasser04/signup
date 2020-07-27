0c37831d727b5e7ee9b17696300af9aac99c1bad
strasserra04 <38735391+strasserra04@users.noreply.github.com>
2020-07-27 14:34:51 -0500

    Initial commit

---
+++ src/components/HeroInfo.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const HeroInfo = () => (
+    <div className="hero-info">
+      <h3>Register for the Education Seminar</h3>
+         <p>
+            We are better together. Join your Fiserv experts and peers for interactive presentations and discussions that provide the necessary insight to become more efficient. Use this face-to-face venue to learn more about the software you use every day and share your best practices and software tips all while building a diverse network of professional relationships.
+         </p>
+         <p>
+            Precision Education Seminars are one and one-half days interactive sessions that provide the ideal setting to gain new perspectives and make valuable connections with peers and Fiserv experts.
+         </p>
+      <h2>Fees</h2>
+         <p class="italic-important">
+         Sign up early to take advantage of a <b>$70 registration discount.</b>
+         </p>
+      <h3>Seminar Fees</h3>
+         <ul>
+            <li>Early registration fee: $565 per attendee through July 31</li>
+            <li>Standard registration fee: $635 per attendee after July 31</li>
+         </ul>
+    </div>
+  );
+  
+  export default HeroInfo;
---
+++ src/utils/seminars.js
@@ -0,0 +1,29 @@
+const Seminars = [
+  {
+    date: "September 9-10",
+    location:
+      "Kansas City, MO"
+  },
+  {
+    date: "September 15-16",
+    location:
+      "Sioux Falls, SD"
+  },
+  {
+    date: "September 22-23",
+    location:
+      "San Antonio, TX "
+  },
+  {
+    date: "October 6-7",
+    location:
+      "Madison, WI"
+  },
+  {
+    date: "October 14-15",
+    location:
+      "Nashville, TN"
+  }
+];
+
+export default Seminars;
