a2136ab7fab25f6bbe0251db6f3be977caddd5e4
strasserra04 <38735391+strasserra04@users.noreply.github.com>
2020-07-31 13:40:34 -0500

    Updates

--- src/components/HeroInfo.js
+++ src/components/HeroInfo.js
@@ -6,10 +6,10 @@ const HeroInfo = () => (
       <div className="hero-info">
       <h3>Register for the Education Seminar</h3>
          <p>
-            We are better together. Join your Fiserv experts and peers for interactive presentations and discussions that provide the necessary insight to become more efficient. Use this face-to-face venue to learn more about the software you use every day and share your best practices and software tips all while building a diverse network of professional relationships.
+            We are better together. Join your Fiserv experts and peers for interactive presentations and discussions that provide the necessary insight to become more efficient. Use this virtual venue to learn more about the software you use every day and share your best practices and software tips all while building a diverse network of professional relationships.
          </p>
          <p>
-            Precision Education Seminars are one and one-half days interactive sessions that provide the ideal setting to gain new perspectives and make valuable connections with peers and Fiserv experts.
+            This fall, the Precision Education Seminars are three days of interactive sessions that provide the ideal setting to gain new perspectives and make valuable connections with peers and Fiserv experts.
          </p>
       <h2>Fees</h2>
          <p class="italic-important">
